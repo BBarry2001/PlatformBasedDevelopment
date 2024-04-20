93eeeb7380c6b388c5625bd3ecb4db42c11252ec
Boubacar Barry <bboubacar98@gmail.com>
2024-04-20 14:42:08 -0500

    Module 5 Commit

---
+++ MainActivity.kt
@@ -0,0 +1,24 @@
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        recyclerView = findViewById(R.id.photoGalleryRecyclerView)
+        recyclerView.layoutManager = GridLayoutManager(this, 3) // 3 columns grid
+        
+        // 'image1', 'image2', 'image3', ... in a folder
+        val imageResourceIds = listOf(
+            R.drawable.image1, R.drawable.image2, R.drawable.image3
+            // Add the rest of your image resource IDs here
+        )
+        
+        recyclerView.adapter = PhotoAdapter(imageResourceIds)
+    }
+}
